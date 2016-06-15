54a4e4a73b30ab3334ec514b2e8c78bd32a1c5f6
julian-printemps <julian.printemps@gmail.com>
2016-06-15 14:46:12 +0900

    Fix ignore

---
+++ wp/wp-content/themes/golf/index.php
@@ -0,0 +1,173 @@
+<?php get_header(); ?>
+
+<!-- Main Section -->
+<section class="section_main">
+  <div class="row">
+    <div class="large-12 columns section_main--inner">
+      <div class="image_wrap">
+          <div class="main_image" style="background-image: url('images/top/main1.jpg');"></div>
+      </div>
+
+      <a href="" class="banner_charity button" title="チャリティー活動">
+        <img src="images/top/charity_ribbon.png" alt="チャリティー活動" width="41" height="50" class="banner_charity--image" />
+        <div class="banner_charity--texts">
+          <p class="text_en">CHARITY</p>
+          <p class="text_ja">チャリティー活動</p>
+        </div>
+      </a>
+
+      <ul class="inpage_menu" data-magellan-expedition="fixed">
+        <li data-magellan-arrival="news"><a href="#news" title="NEWS"><p>NEWS<i class="fa fa-angle-down" aria-hidden="true"></i></p></a></li>
+        <li data-magellan-arrival="about"><a href="#about" title="ABOUT"><p>ABOUT<i class="fa fa-angle-down" aria-hidden="true"></i></p></a></li>
+        <li><a href="#sponsors" title="SPONSORS"><p>SPONSORS<i class="fa fa-angle-down" aria-hidden="true"></i></p></a></li>
+        <li><a href="#sns" title="SNS"><p>SNS<i class="fa fa-angle-down" aria-hidden="true"></i></p></a></li>
+      </ul>
+    </div>
+  </div>
+</section>
+<!-- / Main Section -->
+
+<!-- NEWS Section -->
+<a name="news"></a>
+<section id="news" class="section_news section_in_top" data-magellan-destination="news">
+  <div class="row">
+    <div class="large-12 columns">
+      <h3 class="section_title">NEWS</h3>
+      <ul class="row" class="news_wrap">
+        <li class="small-12 medium-4 columns news_panel">
+          <a href="" title="" class="news_panel--link_box">
+            <div class="news_panel--link_box--inner">
+              <p class="news_panel--date">2016.05.20</p>
+              <p class="news_panel--title">News title</p>
+              <p class="news_panel--text">Display news detail  Display news detail</p>
+            </div>
+          </a>
+        </li>
+        <li class="small-12 medium-4 columns news_panel">
+          <a href="" title="" class="news_panel--link_box">
+            <div class="news_panel--link_box--inner">
+              <p class="news_panel--date">2016.05.20</p>
+              <p class="news_panel--title">News title</p>
+              <p class="news_panel--text">Display news detail</p>
+            </div>
+          </a>
+        </li>
+        <li class="small-12 medium-4 columns news_panel">
+          <a href="" title="" class="news_panel--link_box">
+            <div class="news_panel--link_box--inner">
+              <p class="news_panel--date">2016.05.20</p>
+              <p class="news_panel--title">News title</p>
+              <p class="news_panel--text">Display news detail</p>
+            </div>
+          </a>
+        </li>
+      </ul>
+
+
+      <a href="" title="View All" class="button"><p>View All</p><i class="fa fa-angle-right" aria-hidden="true"></i></a>
+
+    </div>
+  </div>
+</section>
+<!-- / NEWS Section -->
+
+<!-- ABOUT Section -->
+<a name="about" href="#about"></a>
+<section id="about" class="section_about section_in_top" data-magellan-destination="about">
+  <div class="row">
+    <div class="large-12 columns">
+      <h3 class="section_title">ABOUT</h3>
+
+      <ul class="tabs about_list" data-tabs id="about-tabs">
+        <li class="tabs-title about_list--tab is-active" role="presentation">
+          <a href="#panel1" aria-selected="true" class="about_list--tab--link">
+            <img src="images/top/about_profile.png" alt="堀川未来夢 プロフィール" class="about_list--tab--image" />
+            <p class="about_list--tab--title">PROFILE</p>
+          </a>
+        </li>
+        <li class="tabs-title about_list--tab" role="presentation">
+          <a href="#panel2" class="about_list--tab--link">
+            <img src="images/top/about_message.png" alt="堀川未来夢 メッセージ" class="about_list--tab--image" />
+            <p class="about_list--tab--title">MESSAGE</p>
+          </a>
+        </li>
+        <li class="tabs-title about_list--tab" role="presentation">
+          <a href="#panel3" class="about_list--tab--link">
+            <img src="images/top/about_bag.png" alt="堀川未来夢 バッグの中" class="about_list--tab--image" />
+            <p class="about_list--tab--title">IN THE BAG</p>
+          </a>
+        </li>
+      </ul>
+
+      <div class="tabs-content about_content" data-tabs-content="about-tabs">
+        <div class="tabs-panel is-active about_content--panel" id="panel1">
+            <h4 classs="about_content--panel--text">PROFILE</h4>
+            <div class="about_content--panel--description">
+              <h5 class="name_holder"><p class="name">堀川未来夢</p><p class="name_en">Mikumu Horikawa</p></h5>
+              <div class="row">
+                <dl class="small-12 medium-4 columns profile_items profile_items_list">
+                  <dt>生年月日</dt><dd>1992.12.16</dd>
+                  <dt>国籍</dt><dd>日本</dd>
+                  <dt>出身地</dt><dd>神奈川県</dd>
+                  <dt>出身校</dt><dd>日本大学</dd>
+                  <dt>身長</dt><dd>176.0cm</dd>
+                  <dt>体重</dt><dd>78.0kg</dd>
+                </dl>
+                <dl class="small-12 medium-8 columns profile_items profile_items_biography">
+                  <dt>バイオグラフィー</dt>
+                  <dd>既にゴルフを始めていた中学時代はテニス部に入った。力をつけたのは大学進学後。筋力トレーニングに力を入れ、体重は64kgから78kgまで増加した。176cmと背丈は標準だが、がっしりとした体格をしている。ルーキーイヤーとなった2015年のツアー外競技「ネスレ日本マッチプレー選手権 レクサス杯」の１回戦で石川遼を破り、一躍注目を浴びる存在に。「石川遼くん」と他人行儀に呼んでいるが、学年は１つしか違わない。シーズン終盤の「ブリヂストンオープン」で２位になるなど初シードを獲得した。アマチュア時代の2012、13年に国体で２連覇を達成している。</dd>
+                </dl>
+              </div>
+            </div>
+        </div>
+        <div class="tabs-panel about_content--panel" id="panel2">
+          <h4 classs="about_content--panel--text">MESSAGE</h4>
+          <div class="about_content--panel--description">
+            <p>いつも応援してくださっているファンの皆様、サイトを訪れてくださった皆様、ありがとうございます。ファンの皆様に僕のことをもっと知っていただき、僕達若手が男子プロゴルファー界を盛り上げていければと思い、今回このサイトを設立しました。<br />
+            &nbsp;&nbsp;「堀川未来夢」という選手の持ち味は、まず「アプローチパター」。あまり飛距離を飛ばすタイプでもないですし、アクションの大きなゴルフではありませんが、小技を駆使して苦しい局面でもなんとか堅実にパーを積み重ねていく。そして、チャンスが来た時にバーディーを決める。それが僕のプレースタイルです。なので、難コースになればなるほど、特に僕の長所が見ていただけると思います。<br />
+            &nbsp;&nbsp;ゴルフは選手生命の非常に長いスポーツです。僕の最終目標はマスターズで優勝することですが、その為に、まずは「1勝する」ということと、「シードを取る」こと。それを最も頭において日々努力しています。<br />
+            &nbsp;プロゴルファーは、ゴルフのスコアはもちろんですが、何よりファンの皆様の支えが1番大切だと思っています。ですので、僕はファンサービスを常に心がけるようにしています。これからも、試合会場はもちろん、このサイト内においても、僕の試合成績やプライベートな部分の動画なども公開していき、より一層ファンの方々と近い距離感で接することができればと思っています。</p>
+          </div>
+        </div>
+        <div class="tabs-panel about_content--panel" id="panel3">
+          <h4 classs="about_content--panel--text">IN THE BAG</h4>
+          <div class="about_content--panel--description">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- / ABOUT Section -->
+
+<!-- SPONSOR Section -->
+<a name="sponsor" href="#sponsor"></a>
+<section id="sponsor" class="section_sponsor section_in_top">
+  <div class="row">
+    <div class="large-12 columns">
+      <h3 class="section_title">SPONSOR</h3>
+
+      <ul class="sponsor-list">
+        <li class="sponsor">
+          <a href="" title="" class="sponsor--link">
+            <img src="" width="160" height="80" alt="" class="sponsor--image" />
+          </a>
+        </li>
+        <li class="sponsor">
+          <a href="" title="" class="sponsor--link">
+            <img src="" width="160" height="80" alt="" class="sponsor--image" />
+          </a>
+        </li>
+        <li class="sponsor">
+          <a href="" title="" class="sponsor--link">
+            <img src="" width="160" height="80" alt="" class="sponsor--image" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</section>
+<!-- /SPONSOR Section -->
+
+
+<?php get_footer();
