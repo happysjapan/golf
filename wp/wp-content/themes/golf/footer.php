46525ebb26d53384756e597bd4986845845ad126
julian-printemps <julian.printemps@gmail.com>
2016-06-15 17:10:43 +0900

    Adding masonry + fix font size

--- wp/wp-content/themes/golf/footer.php
+++ wp/wp-content/themes/golf/footer.php
@@ -11,6 +11,7 @@
 <!-- Footer Section -->
 
 <script src="<?php echo get_template_directory_uri(); ?>/bower_components/foundation-sites/dist/foundation.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/bower_components/masonry/dist/masonry.pkgd.min.js"></script>
 <!--
 <script src="<?php echo get_template_directory_uri(); ?>/js/min/app.js"></script>
 -->
---
+++ wp/wp-content/themes/golf/page-masonry.php
@@ -0,0 +1,35 @@
+<?php
+/*
+  Template Name: Gallery template
+*/
+get_header(); ?>
+
+<div class="page_wrapper">
+
+  <div class="section_gallery">
+    <div class="row">
+      <div class="columns large-10 large-offset-1">
+        <div class="gallery grid" data-masonry='{ "itemSelector": ".grid-item" }'>
+          <div class="gallery--item grid-item">1</div>
+          <div class="gallery--item grid-item">1</div>
+          <div class="gallery--item grid-item">1</div>
+          <div class="gallery--item grid-item">1</div>
+          <div class="gallery--item grid-item">1</div>
+          <div class="gallery--item grid-item">1</div>
+          <div class="gallery--item grid-item">1</div>
+          <div class="gallery--item grid-item">1</div>
+          <div class="gallery--item grid-item">1</div>
+          <div class="gallery--item grid-item">1</div>
+          <div class="gallery--item grid-item">1</div>
+          <div class="gallery--item grid-item">1</div>
+          <div class="gallery--item grid-item">1</div>
+          <div class="gallery--item grid-item">1</div>
+          <div class="gallery--item grid-item">1</div>
+          <div class="gallery--item grid-item">1</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+<?php get_footer();
