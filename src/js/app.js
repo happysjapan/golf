a30b69a144b12f52b3a197429d0a8fa7572d7949
julian-printemps <julian.printemps@gmail.com>
2016-06-17 17:41:48 +0900

    Fixing issue with motion UI for orbits

--- src/js/app.js
+++ src/js/app.js
@@ -1,31 +1,32 @@
 $(document).foundation();
 
-var lastScrollTop = 0;
-$(window).scroll(function(event){
-   var st = $(this).scrollTop();
-   if (st > lastScrollTop && st > 150){
-       if( !$('#top_header').hasClass('hidden') ){
-         $('#top_header').addClass('hidden');
-         $('#top_header').addClass('logo_hidden');
-       }
-       $('#top_header').removeClass('shown');
-   }
-   else if (st <= 150){
-     $('#top_header').removeClass('hidden');
-     $('#top_header').removeClass('logo_hidden');
-     $('#top_header').removeClass('shown');
-   }
-   else {
-     if( !$('#top_header').hasClass('shown') ){
-       $('#top_header').addClass('shown');
-     }
-     $('#top_header').removeClass('hidden');
-   }
-   lastScrollTop = st;
-});
-
-
 $(function(){
+
+  var lastScrollTop = 0;
+  $(window).scroll(function(event){
+     var st = $(this).scrollTop();
+     if (st > lastScrollTop && st > 150){
+         if( !$('#top_header').hasClass('hidden') ){
+           $('#top_header').addClass('hidden');
+           $('#top_header').addClass('logo_hidden');
+         }
+         $('#top_header').removeClass('shown');
+     }
+     else if (st <= 150){
+       $('#top_header').removeClass('hidden');
+       $('#top_header').removeClass('logo_hidden');
+       $('#top_header').removeClass('shown');
+     }
+     else {
+       if( !$('#top_header').hasClass('shown') ){
+         $('#top_header').addClass('shown');
+       }
+       $('#top_header').removeClass('hidden');
+     }
+     lastScrollTop = st;
+  });
+
+
    // #で始まるアンカーをクリックした場合に処理
    $('a[href^="#"]').click(function() {
 
--- wp/wp-content/themes/golf/single-gallery.php
+++ wp/wp-content/themes/golf/single-gallery.php
@@ -3,13 +3,31 @@ get_header(); ?>
 
 <div class="page_wrapper">
 
-  <div class="section_member">
-    <div class="row">
-      <div class="columns">
-        <?php if ( have_posts() ) { while ( have_posts() ) : the_post(); ?>
-          <?php the_content(); ?>
-        <?php endwhile; } ?>
+  <?php if( have_rows('gallery_slider') ){ ?>
+    <div class="section_slider section_gallery row">
+      <div class="columns small-12 medium-8 medium-offset-2">
+        <div class="gallery_orbit orbit" role="region" aria-label="Favorite Cat Pictures" data-orbit data-options="animInFromLeft:fade-in; animInFromRight:fade-in; animOutToLeft:fade-out; animOutToRight:fade-out;">
+          <button class="no_anim orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
+          <button class="no_anim orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
+
+          <ul class="orbit-container">
+            <?php
+            while ( have_rows('gallery_slider') ) : the_row(); ?>
+              <li class="orbit-slide">
+                <img class="orbit-image" src="<?php echo get_sub_field('gallery_slider_image')['url']; ?>" alt="Space">
+              </li>
+            <?php endwhile; ?>
+          </ul>
+        </div>
       </div>
+    </div>
+  <?php	} ?>
+
+  <div class="section_default section_gallery row">
+    <div class="columns small-12 medium-8 medium-offset-2">
+      <?php if ( have_posts() ) { while ( have_posts() ) : the_post(); ?>
+        <?php the_content(); ?>
+      <?php endwhile; } ?>
     </div>
   </div>
 
