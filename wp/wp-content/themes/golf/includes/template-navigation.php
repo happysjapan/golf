83063d647fb3f3c1886b15e138f364b6b190431f
julian-printemps <julian.printemps@gmail.com>
2016-06-17 12:37:03 +0900

    Template for member page + update signup/in links

--- wp/wp-content/themes/golf/includes/template-navigation.php
+++ wp/wp-content/themes/golf/includes/template-navigation.php
@@ -11,8 +11,8 @@
 
   <div class="sign_menu columns">
     <ul class="dropdown menu sign_menu--menu align-center">
-      <li class="sign_menu--item" role="menuitem"><a class="sign_menu--link" href="#">SIGN UP</a></li>
-      <li class="sign_menu--item" role="menuitem"><a class="sign_menu--link" href="#">SIGN IN</a></li>
+      <li class="sign_menu--item" role="menuitem"><a class="sign_menu--link" href="<?php echo get_permalink(get_page_by_path('sign-up')); ?>">SIGN UP</a></li>
+      <li class="sign_menu--item" role="menuitem"><a class="sign_menu--link" href="<?php echo get_permalink(get_page_by_path('sign-in')); ?>">SIGN IN</a></li>
     </ul>
   </div>
 </nav>
---
+++ wp/wp-content/themes/golf/page-member.php
@@ -0,0 +1,20 @@
+<?php
+/*
+  Template Name: Member page
+*/
+get_header(); ?>
+
+<div class="page_wrapper">
+
+  <div class="section_member">
+    <div class="row">
+      <div class="columns">
+        <?php if ( have_posts() ) { while ( have_posts() ) : the_post(); ?>
+          <?php the_content(); ?>
+        <?php endwhile; } ?>
+      </div>
+    </div>
+  </div>
+
+</div>
+<?php get_footer();
