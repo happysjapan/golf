09ae32bdc30f9bb1f9c199fecbc71df6f6e33052
三光楼茉澄 <sankoro@neopa.jp>
2016-06-23 10:59:13 +0900

    Adding customn fields to top page

--- wp/wp-content/themes/golf/front-page.php
+++ wp/wp-content/themes/golf/front-page.php
@@ -95,37 +95,44 @@ global $post;
             <div class="about_content--panel--description">
               <h5 class="name_holder"><p class="name">堀川未来夢</p><p class="name_en">Mikumu Horikawa</p></h5>
               <div class="row">
-                <dl class="small-12 medium-4 columns profile_items profile_items_list">
+                <dl class="small-12 medium-3 columns profile_items profile_items_list">
                   <dt>生年月日</dt><dd><?php echo get_field('profile_birthday'); ?></dd>
-                  <dt>出身地</dt><dd><?php echo get_field('profile_birthplace'); ?>神奈川県</dd>
-                  <dt>血液型</dt><dd>A型</dd>
-                  <dt>身長</dt><dd>176.0cm</dd>
-                  <dt>体重</dt><dd>78.0kg</dd>
-                  <dt>経歴</dt><dd>テキストテキストテキストテキストテキストテキストテキストテキスト</dd>
+                  <dt>出身地</dt><dd><?php echo get_field('profile_birthplace'); ?></dd>
+                  <dt>血液型</dt><dd><?php echo get_field('profile_bloodtype'); ?></dd>
+                  <dt>身長</dt><dd><?php echo get_field('profile_height'); ?></dd>
+                  <dt>体重</dt><dd><?php echo get_field('profile_weight'); ?></dd>
+                  <dt>経歴</dt><dd><?php echo get_field('profile_school'); ?></dd>
                 </dl>
-                <dl class="small-12 medium-8 columns profile_items profile_items_biography">
+                <dl class="small-12 medium-9 columns profile_items profile_items_biography">
+                  <dt>ゴルフを始めた時期およびきっかけ</dt><dd><?php echo get_field('profile_trigger'); ?></dd>
+                  <dt>中学時代、高校、大学時代の成績</dt><dd><?php echo get_field('profile_result_student'); ?></dd>
+                  <dt>プロデビュー年およびルーキーイヤーの成績</dt><dd><?php echo get_field('profile_result_debut'); ?></dd>
+                  <dt>今年の成績（シーズンスタート）</dt><dd><?php echo get_field('profile_result_thisyear'); ?></dd>
+                </dl>
+                <!-- <dl class="small-12 medium-8 columns profile_items profile_items_biography">
                   <dt>バイオグラフィー</dt>
                   <dd>既にゴルフを始めていた中学時代はテニス部に入った。力をつけたのは大学進学後。筋力トレーニングに力を入れ、体重は64kgから78kgまで増加した。176cmと背丈は標準だが、がっしりとした体格をしている。ルーキーイヤーとなった2015年のツアー外競技「ネスレ日本マッチプレー選手権 レクサス杯」の１回戦で石川遼を破り、一躍注目を浴びる存在に。「石川遼くん」と他人行儀に呼んでいるが、学年は１つしか違わない。シーズン終盤の「ブリヂストンオープン」で２位になるなど初シードを獲得した。アマチュア時代の2012、13年に国体で２連覇を達成している。</dd>
-                </dl>
+                </dl> -->
               </div>
             </div>
         </div>
         <div class="tabs-panel about_content--panel" id="panel2">
           <h4 classs="about_content--panel--text">MESSAGE</h4>
           <div class="about_content--panel--description">
-            <p>いつも応援してくださっているファンの皆様、サイトを訪れてくださった皆様、ありがとうございます。ファンの皆様に僕のことをもっと知っていただき、僕達若手が男子プロゴルファー界を盛り上げていければと思い、今回このサイトを設立しました。<br />
-            &nbsp;&nbsp;「堀川未来夢」という選手の持ち味は、まず「アプローチパター」。あまり飛距離を飛ばすタイプでもないですし、アクションの大きなゴルフではありませんが、小技を駆使して苦しい局面でもなんとか堅実にパーを積み重ねていく。そして、チャンスが来た時にバーディーを決める。それが僕のプレースタイルです。なので、難コースになればなるほど、特に僕の長所が見ていただけると思います。<br />
-            &nbsp;&nbsp;ゴルフは選手生命の非常に長いスポーツです。僕の最終目標はマスターズで優勝することですが、その為に、まずは「1勝する」ということと、「シードを取る」こと。それを最も頭において日々努力しています。<br />
-            &nbsp;プロゴルファーは、ゴルフのスコアはもちろんですが、何よりファンの皆様の支えが1番大切だと思っています。ですので、僕はファンサービスを常に心がけるようにしています。これからも、試合会場はもちろん、このサイト内においても、僕の試合成績やプライベートな部分の動画なども公開していき、より一層ファンの方々と近い距離感で接することができればと思っています。</p>
+            <p><?php echo get_field('message'); ?></p>
           </div>
         </div>
         <div class="tabs-panel about_content--panel" id="panel3">
           <h4 classs="about_content--panel--text">IN THE BAG</h4>
           <div class="about_content--panel--description">
               <div class="row">
-                <dl class="small-12 columns profile_items profile_items_list">
-                  <dt>番手</dt><dd>aaaa</dd>
-                </dl>
+                <dl class="small-12aaaa columns profile_items profile_items_list">
+                  <dt>番手</dt><dd><?php echo get_field('bag_number'); ?><dd>
+                  <dt>モデル</dt><dd><?php echo get_field('bag_model'); ?><dd>
+                  <dt>シャフト</dt><dd><?php echo get_field('bag_shaft'); ?><dd>
+                  <dt>ロフト</dt><dd><?php echo get_field('bag_loft'); ?><dd>
+                  <dt>長さ</dt><dd><?php echo get_field('bag_length'); ?><dd>
+                  <dt>フレックス</dt><dd><?php echo get_field('bag_flex'); ?><dd>
               </div>
           </div>
         </div>
@@ -142,7 +149,7 @@ global $post;
     <div class="large-12 columns">
       <h3 class="section_title">SPONSOR</h3>
 
-      <ul class="sponsor-list">
+      <ul class="sponsor_list">
         <!--  <li class="sponsor">
           <a href="" title="" class="sponsor--link">
             <img src="" width="160" height="80" alt="" class="sponsor--image" />
@@ -160,8 +167,8 @@ global $post;
         </li> -->
 
         <?php while(have_rows('sponsor')): the_row(); ?>
-          <li>
-            <a href="<?php the_sub_field('sponsor_url'); ?>" target="blank"><img src="<?php the_sub_field('sponsor_image'); ?>" alt="<?php the_sub_field('sponsor_name'); ?>" width="160" class="sponsor--image"/></a>
+          <li class="sponsor_list--item">
+            <a href="<?php the_sub_field('sponsor_url'); ?>" target="blank"><img src="<?php the_sub_field('sponsor_image'); ?>" alt="<?php the_sub_field('sponsor_name'); ?>" width="160" class="sponsor_list--image"/></a>
           </li>
         <?php endwhile; ?>
 
