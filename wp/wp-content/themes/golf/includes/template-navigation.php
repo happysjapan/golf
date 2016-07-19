d6371dcfc0e6a030a22407a685597ff06983dc72
julian-printemps <julian.printemps@gmail.com>
2016-07-19 19:32:07 +0900

    update profile

--- wp/wp-content/themes/golf/includes/template-navigation.php
+++ wp/wp-content/themes/golf/includes/template-navigation.php
@@ -14,7 +14,7 @@
       <?php $user = get_currentuserinfo(); ?>
       <h5 class="sign_menu--user"><a class="sign_menu--link" href="<?php echo get_permalink(get_page_by_path('profile')); ?>" title="<?php echo $user->user_nicename; ?>">Hello, <?php echo $user->user_nicename; ?></a></h5>
       <ul class="dropdown menu sign_menu--menu align-left">
-        <li class="sign_menu--item" role="menuitem"><a class="sign_menu--link" href="<?php echo wp_logout_url( home_url() ); ?>">Log out</a></li>
+        <li class="sign_menu--item" role="menuitem"><a class="sign_menu--link" href="<?php echo home_url();?>?swpm-logout=true">Log out</a></li>
       </ul>
     <?php } else { ?>
       <ul class="dropdown menu sign_menu--menu align-center">
