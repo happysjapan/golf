7abf982c94d946dc1b5d250b3ca7007948be1109
julian-printemps <julian.printemps@gmail.com>
2016-07-19 14:35:25 +0900

    Update font + fix login / logout

--- wp/wp-content/themes/golf/includes/template-navigation.php
+++ wp/wp-content/themes/golf/includes/template-navigation.php
@@ -11,8 +11,10 @@
 
   <div class="sign_menu columns">
     <?php if ( is_user_logged_in() ) { ?>
-      <ul class="dropdown menu sign_menu--menu align-center">
-        <li class="sign_menu--item" role="menuitem"><a class="sign_menu--link" href="<?php echo home_url();?>?swpm-logout=true">Log out</a></li>
+      <?php $user = get_currentuserinfo(); ?>
+      <h5 class="sign_menu--user">Hello, <?php echo $user->user_nicename; ?></h5>
+      <ul class="dropdown menu sign_menu--menu align-left">
+        <li class="sign_menu--item" role="menuitem"><a class="sign_menu--link" href="<?php echo wp_logout_url( home_url() ); ?>">Log out</a></li>
       </ul>
     <?php } else { ?>
       <ul class="dropdown menu sign_menu--menu align-center">
