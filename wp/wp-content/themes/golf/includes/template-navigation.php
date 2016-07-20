09df8a927e524a3391899937b335ae6c26c9bc42
三光楼茉澄 <sankoro@neopa.jp>
2016-07-20 13:00:37 +0900

    Fix menu design

--- wp/wp-content/themes/golf/includes/template-navigation.php
+++ wp/wp-content/themes/golf/includes/template-navigation.php
@@ -13,7 +13,7 @@
     <?php if ( is_user_logged_in() ) { ?>
       <?php $user = get_currentuserinfo(); ?>
       <h5 class="sign_menu--user"><a class="sign_menu--link" href="<?php echo get_permalink(get_page_by_path('profile')); ?>" title="<?php echo $user->user_nicename; ?>">Hello, <?php echo $user->user_nicename; ?></a></h5>
-      <ul class="dropdown menu sign_menu--menu align-left">
+      <ul class="dropdown menu sign_menu--menu logout">
         <li class="sign_menu--item" role="menuitem"><a class="sign_menu--link" href="<?php echo wp_logout_url( home_url() ); ?>">Log out</a></li>
       </ul>
     <?php } else { ?>
